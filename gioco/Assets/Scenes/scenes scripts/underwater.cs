fa0183ba5d2f9ab944641666b6c66ba2230b0725
flashrin02 <154712138+flashrin02@users.noreply.github.com>
2025-02-10 23:35:00 +0100

    underwater
    ho fatto l'effetto per quando vai sott'acqua

---
+++ gioco/Assets/Scenes/scenes scripts/underwater.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class UnderwaterDepth : MonoBehaviour
+{
+    [Header("Depth Parameters")]
+    [SerializeField] private Transform mainCamera;
+    [SerializeField] private float depth = 0;
+
+    [Header("Post Processing Volume")]
+    [SerializeField] private Volume postProcessingVolume;
+
+    [Header("Post Processing Profiles")]
+    [SerializeField] private VolumeProfile surfacePostProcessing;
+    [SerializeField] private VolumeProfile underwaterPostProcessing;
+
+    private void Update()
+    {
+        if (mainCamera.position.y < depth)
+        {
+            EnableEffects(true);
+        }
+        else
+        {
+            EnableEffects(false);
+        }
+    }
+
+    private void EnableEffects(bool active)
+    {
+        if (active)
+        {
+            postProcessingVolume.profile = underwaterPostProcessing;
+        }
+        else
+        {
+            postProcessingVolume.profile = surfacePostProcessing;
+        }
+    }
+}
